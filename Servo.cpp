a2c7ccbb07b9d13d60e3109c8739b4cf33884115
William Allison <williama2012@gmail.com>
2024-06-10 21:12:29 -0400

    Settable max range

--- Servo.cpp
+++ Servo.cpp
@@ -9,9 +9,10 @@
  * @retval  None
 **/
 
-void Servo::attach(int pin, int channel, int freq, int resolution) {
+void Servo::attach(int pin, int channel, int freq, int resolution, int servoMax) {
   _channel = channel;
   if(channel > 15) channel = 15;
+  _servomax = servoMax;
   ledcSetup(_channel, freq, resolution);
   ledcAttachPin(pin, channel);
   ledcWrite(_channel, FIRSTDUTY);
@@ -29,15 +30,15 @@ void Servo::detach(uint8_t pin) {
 
 
 /**
- * @brief   writes servo value 0-180 as degree
- * @param   @value: servo value 0-180 as degree
+ * @brief   writes servo value 0-_servomax as degree
+ * @param   @value: servo value 0-_servomax as degree
  * @retval  None
 **/
 
 void Servo::write(int value) {
   if(value < 0) value = 0;
-  if(value > 180) value = 180;
-  int servoValue = (value - SERVOMIN) * (DUTYCYLEMAX - DUTYCYLEMIN) / (SERVOMAX - SERVOMIN) + DUTYCYLEMIN; // mapping to SERVOMIN-SERVOMAX values from DUTYCYLEMIN-DUTYCYLEMAX values
+  if(value > _servomax) value = _servomax;
+  int servoValue = (value - SERVOMIN) * (DUTYCYLEMAX - DUTYCYLEMIN) / (_servomax - SERVOMIN) + DUTYCYLEMIN; // mapping to SERVOMIN-_servomax values from DUTYCYLEMIN-DUTYCYLEMAX values
   ledcWrite(_channel, servoValue);
 
 }
@@ -47,4 +48,11 @@ void Servo::writeMicroseconds(int value) {
   if (value > 3000) value = 3000;
   value = map(value, 0, 3000, 0, 180);
   this->write(value);
+}
+
+void Servo::setServoMax(int value) {
+  if(value <= 0) {
+    value = SERVOMAX;
+  }
+  _servomax = value;
 }
--- Servo.h
+++ Servo.h
@@ -11,18 +11,20 @@
 #define FIRSTDUTY 0
 
 #define SERVOMIN 0
-#define SERVOMAX 180
+#define SERVOMAX 270
 #define DUTYCYLEMIN 100
 #define DUTYCYLEMAX 500
 
 class Servo {
 public:
-  void attach(int pin, int channel = PWMCHANNEL, int freq = PWMFREQ, int resolution = PWMRESOLUTION);
+  void attach(int pin, int channel = PWMCHANNEL, int freq = PWMFREQ, int resolution = PWMRESOLUTION, int servoMax = SERVOMAX);
   void detach(uint8_t pin);
   void write(int value);
   void writeMicroseconds(int value);
+  void setServoMax(int value);
 private:
   int _channel;
+  int _servomax;
 };
 
 #endif
