4d4c6147b1b2b891ab7fdc6b37a972efb0467551
williama2012 <williama2012@gmail.com>
2025-12-14 16:30:57 -0700

    merge to one file

--- esp32_webcontroller.h
+++ esp32_webcontroller.h
@@ -1,9 +1,18 @@
 #ifndef ESP32_CONTROLLER
 #define ESP32_CONTROLLER_H
 #include <Arduino.h>
+#include "esp32_timer.h"
+#include "secrets.h"
+//#include "Servo.h"
+#include "incbin.h"
+#include "Wire.h"
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WebServer.h>
+#include <ESPmDNS.h>
+#include <LiquidCrystal_I2C.h>
 
-
-
+#define SERIAL_BAUDRATE 115200
 
 //enum PinModeEnum {AnalogWrite, AnalogRead, DigitalWrite, DigitalRead, Servo, Tone};
 
@@ -19,7 +28,23 @@ typedef struct {
   int mode; // TODO - AnalogWrite, AnalogRead, DigitalWrite, DigitalRead, Servo, Tone
 } PinMode;
 
-String jsonField(String field, String value, bool addMore);
-String makeStatusItem(int pin, String message, bool includeComma);
+String jsonField(String field, String value, bool addMore) {
+  return "\"" + field + "\":" + "\"" + value + "\"" + (addMore ? "," : "");
+}
+
+String makeStatusItem(int pin, String message, bool includeComma = true) {
+  String msg = "{" + jsonField("pin", String(pin), true) + jsonField("status", message, false) + "}";
+  if (includeComma) {
+    msg += ",";
+  }
+  return msg;
+}
+
+
+
+
+
+
+
 
 #endif
