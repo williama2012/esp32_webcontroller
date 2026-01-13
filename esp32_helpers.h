2a06dca51f762de555670e178c3bc4b0cda4fee6
williama2012 <williama2012@gmail.com>
2026-01-13 12:39:46 -0700

    Update esp32_helpers.h

--- esp32_helpers.h
+++ esp32_helpers.h
@@ -191,7 +191,26 @@ void PrintCore(String msg) {
 
 #pragma endregion Printing
 
+void scan_i2c() {
+  byte error, address;
+  int nDevices = 0;
+  
+  Serial.println("Scanning for I2C devices ...");
+  for (address = 0x01; address < 0x7f; address++) {
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+    if (error == 0) {
+      Serial.printf("I2C device found at address 0x%02X\n", address);
+      nDevices++;
+    } else if (error != 2) {
+      Serial.printf("Error %d at address 0x%02X\n", error, address);
+    }
+  }
+  if (nDevices == 0) {
+    Serial.println("No I2C devices found");
+  }
 
 
+}
 
 #endif
