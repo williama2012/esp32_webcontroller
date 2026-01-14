19cd3ae45609d2760341a39974967baf72ed9fd7
williama2012 <williama2012@gmail.com>
2026-01-13 21:37:04 -0700

    Update esp32_lcd.h

--- esp32_lcd.h
+++ esp32_lcd.h
@@ -7,27 +7,45 @@
 #define LCD_COLS 20
 
 LiquidCrystal_I2C lcd(0x27, LCD_COLS, LCD_ROWS);
+bool lcd_initialized = false;
 
 void lcd_init() {
   lcd.init();
   lcd.backlight();
+  lcd_initialized = true;
 }
 
 void lcd_clear() {
+  if (!lcd_initialized) {
+    return;
+  }
+
   lcd.clear();
 }
 
 void lcd_print(String txt, uint8_t row = 0, uint8_t col = 0) {
+  if (!lcd_initialized) {
+    return;
+  }
+
   lcd.setCursor(col, row);
   lcd.print(txt);
 }
 
 void lcd_print_r(String txt, uint8_t row = 0, uint8_t offset = 0) {
+  if (!lcd_initialized) {
+    return;
+  }
+
   lcd.setCursor(LCD_COLS - txt.length() - offset, row);
   lcd.print(txt);
 }
 
 void lcd_backlight(bool on) {
+  if (!lcd_initialized) {
+    return;
+  }
+
   if (on) {
     lcd.backlight();
   } else {
