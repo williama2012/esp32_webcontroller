2b27fb9b0b6b5682765f3d1ff7267b1d1c2fd107
williama2012 <williama2012@gmail.com>
2026-01-14 10:02:35 -0700

    Update esp32_lcd.h

--- esp32_lcd.h
+++ esp32_lcd.h
@@ -6,12 +6,20 @@
 #define LCD_ROWS 4
 #define LCD_COLS 20
 
+byte HappyFace[] = { B00000, B01010, B01010, B00000, B10001, B01110, B00000, B00000 };
+byte SadFace[] = { B10001, B11011, B01110, B00100, B00100, B01110, B11011, B10001 };
+
 LiquidCrystal_I2C lcd(0x27, LCD_COLS, LCD_ROWS);
 bool lcd_initialized = false;
 
 void lcd_init() {
   lcd.init();
   lcd.backlight();
+  lcd.noCursor();
+  lcd.noBlink();
+
+  lcd.createChar(0, HappyFace);
+  lcd.createChar(1, SadFace);
   lcd_initialized = true;
 }
 
@@ -39,6 +47,24 @@ void lcd_print_r(String txt, uint8_t row = 0, uint8_t offset = 0) {
 
   lcd.setCursor(LCD_COLS - txt.length() - offset, row);
   lcd.print(txt);
+}
+
+void lcd_write(uint8_t char_id, uint8_t row = 0, uint8_t col = 0) {
+  if (!lcd_initialized) {
+    return;
+  }
+
+  lcd.setCursor(col, row);
+  lcd.write(char_id);
+}
+
+void lcd_write_r(uint8_t char_id, uint8_t row = 0, uint8_t offset = 0) {
+  if (!lcd_initialized) {
+    return;
+  }
+
+  lcd.setCursor(LCD_COLS - 1 - offset, row);
+  lcd.write(char_id);
 }
 
 void lcd_backlight(bool on) {
