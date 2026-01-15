bf51153a2a49157904c2b53f8986495f3d1efbdb
williama2012 <williama2012@gmail.com>
2026-01-15 13:49:14 -0700

    Update esp32_helpers.h

--- esp32_helpers.h
+++ esp32_helpers.h
@@ -191,20 +191,20 @@ byte *scan_i2c() {
 
   byte *array = (byte *)malloc(0x7f * sizeof(byte));
 
-  Serial.println("Scanning for I2C devices ...");
+  Serial.println(F("Scanning for I2C devices ..."));
   for (address = 0x01; address < 0x7f; address++) {  //0x7f
     Wire.beginTransmission(address);
     error = Wire.endTransmission();
     if (error == 0) {
-      Serial.printf("I2C device found at address 0x%02X\n", address);
+      Serial.printf(F("I2C device found at address 0x%02X\n"), address);
       array[nDevices] = address;
       nDevices++;
     } else if (error != 2) {
-      Serial.printf("Error %d at address 0x%02X\n", error, address);
+      Serial.printf(F("Error %d at address 0x%02X\n"), error, address);
     }
   }
   if (nDevices == 0) {
-    Serial.println("No I2C devices found");
+    Serial.println(F("No I2C devices found"));
   }
 
   return array;
