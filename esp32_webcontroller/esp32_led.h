76ebd9afab435e8719297abd943a2b4fe22e80e5
williama2012 <williama2012@gmail.com>
2026-01-17 13:27:47 -0700

    Update esp32_led.h
    Option to hold from refreshing to allow setting multiple pixels before updating the LEDs

--- esp32_webcontroller/esp32_led.h
+++ esp32_webcontroller/esp32_led.h
@@ -1,5 +1,5 @@
-#ifndef ESP32_LEDSTRIP_H
-#define ESP32_LEDSTRIP_H
+#ifndef ESP32_LED_H
+#define ESP32_LED_H
 #include <Arduino.h>
 #include <FastLED.h>
 
@@ -8,12 +8,10 @@
 #define NUM_LEDS    484
 #define LED_TYPE    WS2812B
 #define COLOR_ORDER GRB
-
 #define LED_COUNT 484
-
 CRGB leds[NUM_LEDS];
 
-
+bool led_initialized = false;
 uint8_t flip = 1;
 uint16_t color_weight = 1;
 uint16_t color_r = 0;
@@ -21,6 +19,7 @@ uint16_t color_g = 0;
 uint16_t color_b = 0;
 bool color_up = true;
 
+#pragma region Pixel XY coordination
 
 // Params for width and height
 const uint16_t kMatrixWidth = 22;
@@ -63,6 +62,7 @@ uint16_t XY( uint8_t x, uint8_t y) {
   return i;
 }
 
+#pragma endregion Pixel XY coordination
 
 void led_clear() {
   fill_solid(leds, NUM_LEDS, CRGB(0,0,0));
@@ -72,40 +72,49 @@ void led_clear() {
 void led_init() {
     FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS);
     FastLED.setBrightness(100);
+    led_initialized = true;
     led_clear();
 }
 
 void set_brightness(uint16_t brightness) {
+  if (!led_initialized) { return; }
+
   FastLED.setBrightness(brightness);
   FastLED.show();
 }
 
 void set_brightnessP(uint16_t percent) {
+  if (!led_initialized) { return; }
+
   uint16_t val = map(percent, 0, 100, 0, 255);
   FastLED.setBrightness(val);
   FastLED.show();
 }
 
-void set_pixel_i(uint16_t i, CRGB color) {
+void set_pixel_i(uint16_t i, CRGB color, bool hold_refresh = false) {
+  if (!led_initialized) { return; }
+
   if (i >= LED_COUNT) {
     i = LED_COUNT - 1;
   }
 
   leds[i] = color;
-  FastLED.show();
+  if (!hold_refresh) {
+    FastLED.show();
+  }
 }
 
-void set_pixel_i(uint16_t i, uint16_t r, uint16_t g, uint16_t b) {
-  set_pixel_i(i, CRGB(r, g, b));
+void set_pixel_i(uint16_t i, uint16_t r, uint16_t g, uint16_t b, bool hold_refresh = false) {
+  set_pixel_i(i, CRGB(r, g, b), hold_refresh);
 }
 
-void set_pixel(uint16_t x, uint16_t y, CRGB color = CRGB::White) {
+void set_pixel(uint16_t x, uint16_t y, CRGB color = CRGB::White, bool hold_refresh = false) {
   uint16_t i = XY(x, y);
-  set_pixel_i(i, color);
+  set_pixel_i(i, color, hold_refresh);
 }
 
-void set_pixel(uint16_t x, uint16_t y, uint16_t r, uint16_t g, uint16_t b) {
-  set_pixel(x, y, CRGB(r, g, b));
+void set_pixel(uint16_t x, uint16_t y, uint16_t r, uint16_t g, uint16_t b, bool hold_refresh = false) {
+  set_pixel(x, y, CRGB(r, g, b), hold_refresh);
 }
 
 CRGB led_color(String color) {
@@ -157,6 +166,7 @@ void step_weight() {
 }
 
 void setAllColor(CRGB color) {
+  if (!led_initialized) { return; }
   fill_solid(leds, NUM_LEDS, color);
   FastLED.show();
 }
@@ -166,6 +176,7 @@ void setAllColor(uint16_t r, uint16_t g, uint16_t b) {
 }
 
 void setAllColorSequence(uint16_t red, uint16_t green, uint16_t blue, uint32_t wait = 10) {
+  if (!led_initialized) { return; }
   for(int i = 0; i < LED_COUNT; i++) {
     leds[i] = CRGB(red, green, blue);
     FastLED.show();
