a5c09d56462e73d7b057ac441ead3eceddb0a214
williama2012 <williama2012@gmail.com>
2026-07-18 15:43:27 -0700

    Update esp32_led.h
    replacing fastled with neopixel

--- esp32_webcontroller/esp32_led.h
+++ esp32_webcontroller/esp32_led.h
@@ -1,189 +1,77 @@
 #ifndef ESP32_LED_H
 #define ESP32_LED_H
-#include <Arduino.h>
-#include <FastLED.h>
 
-// Configuration
-#define LED_PIN     22
-#define NUM_LEDS    484
-#define LED_TYPE    WS2812B
-#define COLOR_ORDER GRB
-#define LED_COUNT 484
-CRGB leds[NUM_LEDS];
 
-bool led_initialized = false;
-uint8_t flip = 1;
-uint16_t color_weight = 1;
-uint16_t color_r = 0;
-uint16_t color_g = 0;
-uint16_t color_b = 0;
-bool color_up = true;
+// Adafruit_NeoMatrix example for single NeoPixel Shield.
+// Scrolls 'Howdy' across the matrix in a portrait (vertical) orientation.
 
-#pragma region Pixel XY coordination
+#include <Adafruit_GFX.h>
+#include <Adafruit_NeoMatrix.h>
+#include <Adafruit_NeoPixel.h>
+#ifndef PSTR
+ #define PSTR // Make Arduino Due happy
+#endif
 
-// Params for width and height
-const uint16_t kMatrixWidth = 22;
-const uint16_t kMatrixHeight = 22;
+#define PIN 14
 
-// Param for different pixel layouts
-const bool    kMatrixSerpentineLayout = true;
-const bool    kMatrixVertical = false;
+// MATRIX DECLARATION:
+// Parameter 1 = width of NeoPixel matrix
+// Parameter 2 = height of matrix
+// Parameter 3 = pin number (most are valid)
+// Parameter 4 = matrix layout flags, add together as needed:
+//   NEO_MATRIX_TOP, NEO_MATRIX_BOTTOM, NEO_MATRIX_LEFT, NEO_MATRIX_RIGHT:
+//     Position of the FIRST LED in the matrix; pick two, e.g.
+//     NEO_MATRIX_TOP + NEO_MATRIX_LEFT for the top-left corner.
+//   NEO_MATRIX_ROWS, NEO_MATRIX_COLUMNS: LEDs are arranged in horizontal
+//     rows or in vertical columns, respectively; pick one or the other.
+//   NEO_MATRIX_PROGRESSIVE, NEO_MATRIX_ZIGZAG: all rows/columns proceed
+//     in the same order, or alternate lines reverse direction; pick one.
+//   See example below for these values in action.
+// Parameter 5 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_GRBW    Pixels are wired for GRBW bitstream (RGB+W NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
 
-uint16_t XY( uint8_t x, uint8_t y) {
-  uint16_t i;
-  
-  if( kMatrixSerpentineLayout == false) {
-    if (kMatrixVertical == false) {
-      i = (y * kMatrixWidth) + x;
-    } else {
-      i = kMatrixHeight * (kMatrixWidth - (x+1))+y;
-    }
+
+// Example for NeoPixel Shield.  In this application we'd like to use it
+// as a 5x8 tall matrix, with the USB port positioned at the top of the
+// Arduino.  When held that way, the first pixel is at the top right, and
+// lines are arranged in columns, progressive order.  The shield uses
+// 800 KHz (v2) pixels that expect GRB color data.
+Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(22, 22, PIN,
+  NEO_MATRIX_TOP     + NEO_MATRIX_LEFT +
+  NEO_MATRIX_ROWS + NEO_MATRIX_ZIGZAG,
+  NEO_GRB            + NEO_KHZ800);
+
+const uint16_t colors[] = {
+  matrix.Color(255, 0, 0), matrix.Color(0, 255, 0), matrix.Color(0, 0, 255) };
+
+void setup_leds() {
+  matrix.begin();
+  matrix.setTextWrap(false);
+  matrix.setBrightness(40);
+  matrix.setTextColor(colors[0]);
+}
+
+int x    = matrix.width();
+int pass = 0;
+
+void loop_leds() {
+  matrix.fillScreen(0);
+  matrix.setCursor(x, 0);
+  matrix.print(F("Mr.Big"));
+  if(--x < -36) {
+    x = matrix.width();
+    if(++pass >= 3) pass = 0;
+    matrix.setTextColor(colors[pass]);
   }
-
-  if( kMatrixSerpentineLayout == true) {
-    if (kMatrixVertical == false) {
-      if( y & 0x01) {
-        // Odd rows run backwards
-        uint8_t reverseX = (kMatrixWidth - 1) - x;
-        i = (y * kMatrixWidth) + reverseX;
-      } else {
-        // Even rows run forwards
-        i = (y * kMatrixWidth) + x;
-      }
-    } else { // vertical positioning
-      if ( x & 0x01) {
-        i = kMatrixHeight * (kMatrixWidth - (x+1))+y;
-      } else {
-        i = kMatrixHeight * (kMatrixWidth - x) - (y+1);
-      }
-    }
-  }
-  
-  return i;
+  matrix.show();
+  delay(100);
 }
 
-#pragma endregion Pixel XY coordination
 
-void led_clear() {
-  if (!led_initialized) { return; }
-  fill_solid(leds, NUM_LEDS, CRGB(0,0,0));
-  FastLED.show();
-}
 
-void led_init() {
-    FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS);
-    FastLED.setBrightness(100);
-    led_initialized = true;
-    led_clear();
-}
-
-void set_brightness(uint16_t brightness) {
-  if (!led_initialized) { return; }
-
-  FastLED.setBrightness(brightness);
-  FastLED.show();
-}
-
-void set_brightnessP(uint16_t percent) {
-  if (!led_initialized) { return; }
-
-  uint16_t val = map(percent, 0, 100, 0, 255);
-  FastLED.setBrightness(val);
-  FastLED.show();
-}
-
-void set_pixel_i(uint16_t i, CRGB color, bool hold_refresh = false) {
-  if (!led_initialized) { return; }
-
-  if (i >= LED_COUNT) {
-    i = LED_COUNT - 1;
-  }
-
-  leds[i] = color;
-  
-  if (!hold_refresh) {
-    FastLED.show();
-  }
-}
-
-void set_pixel_i(uint16_t i, uint16_t r, uint16_t g, uint16_t b, bool hold_refresh = false) {
-  set_pixel_i(i, CRGB(r, g, b), hold_refresh);
-}
-
-void set_pixel(uint16_t x, uint16_t y, CRGB color = CRGB::White, bool hold_refresh = false) {
-  uint16_t i = XY(x, y);
-  set_pixel_i(i, color, hold_refresh);
-}
-
-void set_pixel(uint16_t x, uint16_t y, uint16_t r, uint16_t g, uint16_t b, bool hold_refresh = false) {
-  set_pixel(x, y, CRGB(r, g, b), hold_refresh);
-}
-
-CRGB led_color(String color) {
-  Serial.println(color);
-  color.toLowerCase();
-
-  if (color == "white") {
-    return CRGB::White;
-  }
-  if (color == "red") {
-    return CRGB::Red;
-  }
-  if (color == "green") {
-    return CRGB::Green;
-  }
-  if (color == "blue") {
-    return CRGB::Blue;
-  }
-  if (color == "Black") {
-    return CRGB::Black;
-  }
-
-  return CRGB::Black;
-}
-
-void flip_color() {
-  if (flip == 1) {
-    flip = 2;
-  } else {
-    flip = 1;
-  }
-}
-
-void step_weight() {
-  if (color_up) {
-    color_weight = color_weight + 1;
-    if (color_weight >= 255) {
-      color_weight = 255;
-      color_up = false;
-    }
-  } else {
-    color_weight = color_weight - 1;
-    if (color_weight <= 1) {
-      color_weight = 1;
-      flip_color();
-      color_up = true;
-    }
-  }
-}
-
-void setAllColor(CRGB color) {
-  if (!led_initialized) { return; }
-  fill_solid(leds, NUM_LEDS, color);
-  FastLED.show();
-}
-
-void setAllColor(uint16_t r, uint16_t g, uint16_t b) {
-  setAllColor(CRGB(r, g, b));
-}
-
-void setAllColorSequence(uint16_t red, uint16_t green, uint16_t blue, uint32_t wait = 10) {
-  if (!led_initialized) { return; }
-  for(int i = 0; i < LED_COUNT; i++) {
-    leds[i] = CRGB(red, green, blue);
-    FastLED.show();
-    delay(wait);
-  }
-}
 
 #endif
